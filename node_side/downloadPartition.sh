0d27922ab568a951697c5ce23e1a42890e96a33b
Jason Pinelo <st1809139@upy.edu.mx>
2021-07-26 15:14:18 -0500

    Changing IP of leader

--- node_side/downloadPartition.sh
+++ node_side/downloadPartition.sh
@@ -6,7 +6,7 @@ DOWN_FILE=$2
 mkdir partition
 mkdir processed
 
-scp -i ~/.ssh/$USER $USER@34.133.161.216:covid_project/files/$DOWN_FILE ./partition
+scp -i ~/.ssh/$USER $USER@34.122.137.68:covid_project/files/$DOWN_FILE ./partition
 cd ./partition
 unzip $DOWN_FILE
 rm $DOWN_FILE
--- node_side/uploadPartition.sh
+++ node_side/uploadPartition.sh
@@ -5,7 +5,7 @@ DOWN_FILE=$2
 UP_FILE=$3
 
 
-scp -i ~/.ssh/$USER ./processed/$UP_FILE $USER@34.133.161.216:covid_project/processedFiles
+scp -i ~/.ssh/$USER ./processed/$UP_FILE $USER@34.122.137.68:covid_project/processedFiles
 rm ./partition/$DOWN_FILE
 rm ./processed/$UP_FILE
 rm -rf partition
